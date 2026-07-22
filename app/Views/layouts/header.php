a3dccfbf971c8dd60874587d30adab25bfa42e52
Ilesanmi Erioluwa Victor <ilesanmierioluwavictor@gmail.com>
2026-07-22 11:58:41 +0100

    Add SVG favicon (notice board icon)

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="<?= htmlspecialchars(\App\Core\Auth::getCsrfToken()) ?>">
     <title>Digital Notice Board</title>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <?php
